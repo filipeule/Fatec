b659fe2a4748f0e61ab4891c2b0309c2e58493a3
LinekerCalseverini <lineker.calseverini@gmail.com>
2023-06-16 18:55:37 -0300

    Criada página de listagem turmas

--- courses/classes/routines/insertclass.php
+++ courses/classes/routines/insertclass.php
@@ -5,27 +5,30 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/sistema_corno/common/dbconnection.php
 
 ?>
 
-<?php if (isset($_POST['nome']) && isset($_POST['id_nivel']) && isset($_POST['id_duracao_ciclo']) && isset($_POST['qtd_ciclos']) && isset($_POST['id_status'])) : ?>
-  <?php
-  $sql = "INSERT INTO cursos (nome, id_nivel, id_duracao_ciclo, qtd_ciclos, id_status) VALUES (?, ?, ?, ?, ?)";
+<?php if (isset($_POST['id_periodo']) && isset($_POST['id_curso']) && isset($_POST['nr_ciclo']) && isset($_POST['id_local'])): ?>
+   <?php
+   $sql = "INSERT INTO turmas (id_periodo, ciclo, id_curso, id_local) VALUES (?, ?, ?, ?)";
 
-  $stmt = $conn->prepare($sql);
-  $stmt->bindParam(1, $_POST['nome'], PDO::PARAM_STR);
-  $stmt->bindParam(2, $_POST['id_nivel'], PDO::PARAM_STR);
-  $stmt->bindParam(3, $_POST['id_duracao_ciclo'], PDO::PARAM_STR);
-  $stmt->bindParam(4, $_POST['qtd_ciclos'], PDO::PARAM_STR);
-  $stmt->bindParam(5, $_POST['id_status'], PDO::PARAM_STR);
-  $success = $stmt->execute();
+   $stmt = $conn->prepare($sql);
+   $stmt->bindParam(1, $_POST['id_periodo'], PDO::PARAM_STR);
+   $stmt->bindParam(2, $_POST['id_curso'], PDO::PARAM_STR);
+   $stmt->bindParam(3, $_POST['nr_ciclo'], PDO::PARAM_STR);
+   $stmt->bindParam(4, $_POST['id_local'], PDO::PARAM_STR);
+   $success = $stmt->execute();
 
-  ?>
-  <?php if ($success) : ?>
-    <p>Salvo com sucesso. Clique <a class="linkVoltar" href="/sistema_corno/courses/info/coursecreate.php">aqui</a> para voltar.</p>
-  <?php else : ?>
-    <p>Erro ao criar curso: <?= $stmt->error ?></p>
-  <?php endif; ?>
+   ?>
+   <?php if ($success): ?>
+      <p>Salvo com sucesso. Clique <a class="linkVoltar" href="/sistema_corno/courses/classes/classcreate.php">aqui</a> para
+         voltar.</p>
+   <?php else: ?>
+      <p>Erro ao criar curso:
+         <?= $stmt->error ?>
+      </p>
+   <?php endif; ?>
 
-<?php else : ?>
-  <p>400 - Bad Request. Clique <a class="linkVoltar" href="/sistema_corno/courses/info/coursecreate.php">aqui</a> para voltar.</p>
+<?php else: ?>
+   <p>400 - Bad Request. Clique <a class="linkVoltar" href="/sistema_corno/courses/classes/coursecreate.php">aqui</a> para
+      voltar.</p>
 <?php endif; ?>
 
 <?php
